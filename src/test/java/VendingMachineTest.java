b2b38675161d17e65bc2c897d89d94e8b46d6d43
Toshiyuki Ando <couger@kt.rim.or.jp>
2020-02-28 21:16:48 +0900

    導通確認のテスト

---
+++ src/test/java/VendingMachineTest.java
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class VendingMachineTest {
+    @Test
+    void first_test() {
+        assertEquals(1,1);
+    }
+}
