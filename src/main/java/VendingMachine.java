34f8668e5da9219d639d7e879215691589f49501
Toshiyuki Ando <couger@kt.rim.or.jp>
2020-02-28 21:37:24 +0900

    簡単なシナリオを追加 (100円で水を買うと水が出てくる)

---
+++ src/main/java/VendingMachine.java
@@ -0,0 +1,12 @@
+public class VendingMachine {
+    public String dispensingSlot = "water";
+    public int changePort;
+
+    public void insetMoney(int money) {
+
+    }
+
+    public void purchase(String drink) {
+
+    }
+}
--- src/test/java/VendingMachineTest.java
+++ src/test/java/VendingMachineTest.java
@@ -5,6 +5,10 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class VendingMachineTest {
     @Test
     void first_test() {
-        assertEquals(1,1);
+        VendingMachine machine = new VendingMachine();
+        machine.insetMoney(100);
+        machine.purchase("water");
+        assertEquals("water", machine.dispensingSlot);
+        assertEquals(0, machine.changePort);
     }
 }
